5c8b2a1bd7d747ed0754ab36fe7cee902468bc89
Koy <koy@ko8e24.top>
2020-10-21 15:05:13 +0800

    [update] update IndexController example.

--- src/main/java/com/koy/kono/app/controller/IndexController.java
+++ src/main/java/com/koy/kono/app/controller/IndexController.java
@@ -1,4 +1,21 @@
 package com.koy.kono.app.controller;
 
-public class IndexController implements IController {
+import com.koy.kono.kono.core.BaseController;
+
+import java.util.*;
+
+/**
+ * @author Koy  https://github.com/Koooooo-7
+ * @Description
+ */
+
+public class IndexController extends BaseController {
+
+    public void user() {
+        Optional<String> user = this.getRequest().get("user");
+        Map<String, Object> map = new HashMap<>();
+        map.put("Koy", "Hello World!");
+        map.put(user.orElseGet(() -> "Anonymous"), "Kono!");
+        this.getResponse().send(map).json();
+    }
 }
