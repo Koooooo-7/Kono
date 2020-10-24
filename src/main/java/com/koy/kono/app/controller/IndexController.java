1dfde564b56c6755b6ad5bcd05e33c5837116b2e
Koy <koy@ko8e24.top>
2020-10-24 15:58:19 +0800

    [update example] update example.

--- src/main/java/com/koy/kono/app/controller/IndexController.java
+++ src/main/java/com/koy/kono/app/controller/IndexController.java
@@ -6,16 +6,26 @@ import java.util.*;
 
 /**
  * @author Koy  https://github.com/Koooooo-7
- * @Description
+ * @Description Example, the default route is /index.
  */
 
 public class IndexController extends BaseController {
 
+    // both support the GET and POST request to /index/user
     public void user() {
         Optional<String> user = this.getRequest().get("user");
         Map<String, Object> map = new HashMap<>();
         map.put("Koy", "Hello World!");
         map.put(user.orElseGet(() -> "Anonymous"), "Kono!");
+        this.getResponse().send(map).json();
+    }
+
+    // only support the GET request to /index/user, the specific request type method has higher priority.
+    public void getUser(){
+        Optional<String> user = this.getRequest().get("user");
+        Map<String, Object> map = new HashMap<>();
+        map.put("Koy2", "Hello World!2");
+        map.put(user.orElseGet(() -> "Anonymous2"), "Kono!2");
         this.getResponse().send(map).json();
     }
 }
