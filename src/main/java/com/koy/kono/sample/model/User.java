7faca089997de94fd63a00823caa07567efae438
Koy <koy@ko8e24.top>
2020-12-08 22:36:49 +0800

    [update] // TODO: wrapper ORM.

--- src/main/java/com/koy/kono/sample/model/User.java
+++ src/main/java/com/koy/kono/sample/model/User.java
@@ -1,6 +1,8 @@
 package com.koy.kono.sample.model;
 
 import com.koy.kono.kono.model.BaseModel;
+import org.jooq.Record;
+import org.jooq.Result;
 
 /**
  * @author Koy  https://github.com/Koooooo-7
@@ -12,5 +14,7 @@ public class User extends BaseModel {
     private Integer id;
     private String name;
 
-
+    private void getUsers() {
+        Result<Record> recordResult = this.DB().select().from("user").fetch();
+    }
 }
