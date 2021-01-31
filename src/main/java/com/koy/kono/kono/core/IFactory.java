5a2659fa57e50e202972b8652f3e72a75b21b0b5
Koy <koy@ko8e24.top>
2021-01-31 10:25:09 +0800

    fix(build): fix ci error.

---
+++ src/main/java/com/koy/kono/kono/core/IFactory.java
@@ -0,0 +1,10 @@
+package com.koy.kono.kono.core;
+
+/**
+ * @author Koy  https://github.com/Koooooo-7
+ * @Description
+ */
+
+public interface IFactory {
+    void load(DefaultClassLoader defaultClassLoader, Configuration configuration);
+}
---
+++ src/main/java/com/koy/kono/kono/core/IRouter.java
@@ -0,0 +1,9 @@
+package com.koy.kono.kono.core;
+
+/**
+ * @author Koy  https://github.com/Koooooo-7
+ * @Description
+ */
+
+public interface IRouter {
+}
